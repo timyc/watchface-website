cd2479d4054f497c2a2dd7ae0f939bc368c12ed8
Tim Chen <29265905+timyc@users.noreply.github.com>
2023-01-09 19:44:32 -0800

    initialize Vue with basic layout

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { useSettingsStore } from '@/stores/settings';
+export default {
+  name: 'HomeView',
+  setup() {
+    const settingsStore = useSettingsStore();
+    return {
+      settingsStore,
+    };
+  },
+}
+</script>
+
+<template>
+  <!-- Fitbit image -->
+  <div class="h-50 w-100 d-abs d-flex">
+    <img src="@/assets/fitbit.png"
+      style="max-height: 100%; max-width: 100%; width: 100%; height: auto; object-fit: contain;" />
+  </div>
+  <!-- Whatever is inside the Fitbit should be added below -->
+  <div class="center h-50 d-flex">test</div>
+  <!-- Last row for option choosing -->
+  <div class="h-50 center d-flex">
+    last row
+  </div>
+</template>
+
+<style scoped>
+img {
+  pointer-events: none;
+}
+</style>
