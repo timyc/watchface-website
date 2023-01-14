9e73674fe30d102d8e9ba3a0f7a9a575160c3835
Tim Chen <29265905+timyc@users.noreply.github.com>
2023-01-14 01:24:07 -0800

    also dynamic-ize this theme

--- src/components/themes/ModernSleek.vue
+++ src/components/themes/ModernSleek.vue
@@ -1,10 +1,16 @@
+<script setup lang="ts">
+import { useSettingsStore } from '@/stores/settings';
+import aesthetics from '@/data/aesthetics';
+const settingsStore = useSettingsStore();
+</script>
+
 <template>
     <svg width="250" height="250" viewBox="0 0 3 3" xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink">
         <defs>
             <radialGradient id="rgrad">
-                <stop offset="5%" stop-color="#2CB574" />
-                <stop offset="95%" stop-color="black" />
+                <stop offset="5%" :stop-color="settingsStore.aesthetic == null ? '#2CB574' : aesthetics[settingsStore.aesthetic as keyof typeof aesthetics].theme[0]" />
+                <stop offset="95%" :stop-color="settingsStore.aesthetic == null ? 'black' : aesthetics[settingsStore.aesthetic as keyof typeof aesthetics].theme[1]" />
             </radialGradient>
         </defs>
         <rect width="100%" height="100%" rx="1" fill="url('#rgrad')" />
