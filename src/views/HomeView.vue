e4e97dcea4fdb6ce8c7c64d112d1d1172fd32449
Tim Chen <29265905+timyc@users.noreply.github.com>
2023-01-20 15:04:08 -0800

    add the menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,38 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, ref } from 'vue';
 import { useSettingsStore } from '@/stores/settings';
 import aesthetics from '@/data/aesthetics';
 import layouts from '@/data/layouts';
 const settingsStore = useSettingsStore();
 const importer = (type: string, file: string) => defineAsyncComponent(() => import(`@/components/${type}/${file}.vue`));
+const menuOpen = ref(false);
 </script>
 
 <template>
-  <!-- Fitbit outer svg -->
-  <div class="center h-50 d-abs d-flex" style="margin-left: auto; margin-right: auto; right: 0; left: 0">
-    <svg width="250" height="250">
-      <rect width="100%" height="100%" rx="50" ry="50" stroke="black" stroke-width="30" fill-opacity="0" />
-    </svg>
+  <div :class="{'blur': menuOpen}" style="height:400px;position:sticky;top:0">
+    <nav style="margin:10px">
+      <div id="menu-button" class="right d-table" @click="menuOpen = true"><img class="d-cell" src="/icons/menu.png" width="20" /><span
+          class="d-cell" style="vertical-align: middle">Menu</span></div>
+    </nav>
+    <!-- Fitbit outer svg -->
+    <div class="center h-50 d-abs d-flex" style="margin-left: auto; margin-right: auto; right: 0; left: 0">
+      <svg width="250" height="250">
+        <rect width="100%" height="100%" rx="50" ry="50" stroke="black" stroke-width="30" fill-opacity="0" />
+      </svg>
+    </div>
+    <!-- Whatever is inside the Fitbit should be added below -->
+    <div class="center h-50 d-flex"
+      :style="{ 'font-family': settingsStore.aesthetic == null ? 'inherit' : `${aesthetics[settingsStore.aesthetic as keyof typeof aesthetics].layout[0]}` }">
+      <component
+        :is="settingsStore.theme == null ? importer('themes', 'NoTheme') : importer('themes', settingsStore.theme)">
+        <component
+          :is="settingsStore.layout == null ? importer('layouts', 'NoLayout') : importer('layouts', `${layouts[settingsStore.layout as keyof typeof layouts].type}Layout`)" />
+      </component>
+    </div>
   </div>
-  <!-- Whatever is inside the Fitbit should be added below -->
-  <div class="center h-50 d-flex" :style="{'font-family': settingsStore.aesthetic == null ? 'inherit' : `${aesthetics[settingsStore.aesthetic as keyof typeof aesthetics].layout[0]}`}">
-    <component :is="settingsStore.theme == null ? importer('themes', 'NoTheme') : importer('themes', settingsStore.theme)">
-      <component :is="settingsStore.layout == null ? importer('layouts', 'NoLayout') : importer('layouts', `${layouts[settingsStore.layout as keyof typeof layouts].type}Layout`)" />
-    </component>
-  </div>
+
   <!-- Last row for option choosing -->
-  <div class="center d-flex">
+  <div :class="{'blur': menuOpen}" class="center d-flex">
     <IntroStep v-if="settingsStore.step == 1" />
     <CheckpointStep :num="1" text="Customize your data" v-if="settingsStore.step == 2" />
     <LayoutStep v-if="settingsStore.step == 3" />
@@ -32,11 +43,49 @@ const importer = (type: string, file: string) => defineAsyncComponent(() => impo
     <AestheticStep v-if="settingsStore.step == 8" />
     <ExportStep v-if="settingsStore.step == 9" />
   </div>
+  <div class="d-abs w-100" v-if="menuOpen" style="top:0;">
+    <div id="menu" class="d-table">
+      <img class="d-cell clickable" src="/icons/close.png" width="20" style="margin-left: auto" @click="menuOpen = false" />
+      <div class="d-table w-100p">
+        <div class="menuOption clickable d-cell" @click="settingsStore.step = 4;menuOpen = false">Data</div>
+        <div class="d-cell right" v-if="settingsStore.fields.length > 0"><img src="/icons/checkmark.png" width="20" /></div>
+      </div>
+      <div class="d-table w-100p">
+        <div class="menuOption clickable" @click="settingsStore.step = 6;menuOpen = false">Theme</div>
+        <div class="d-cell right" v-if="settingsStore.theme != null"><img src="/icons/checkmark.png" width="20" /></div>
+      </div>
+      <div class="d-table w-100p">
+        <div class="menuOption clickable" @click="settingsStore.step = 3;menuOpen = false">Layout</div>
+        <div class="d-cell right" v-if="settingsStore.layout != null"><img src="/icons/checkmark.png" width="20" /></div>
+      </div>
+      <div class="d-table w-100p">
+        <div class="menuOption clickable" @click="settingsStore.step = 8;menuOpen = false">Aesthetic</div>
+        <div class="d-cell right" v-if="settingsStore.aesthetic != null"><img src="/icons/checkmark.png" width="20" /></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-img {
-  pointer-events: none;
-  user-select: none;
+#menu-button {
+  padding: 10px;
+  background: #D9D9D9;
+  width: 70px;
+  margin-left: auto;
+  border-radius: 10px;
+  cursor: pointer;
+}
+#menu {
+  background: #D9D9D9;
+  padding:20px;
+  margin-left:auto;
+  width:200px;
+  height:200px;
+}
+.menuOption {
+  padding: 10px;
+}
+.blur {
+  filter: blur(5px);
 }
 </style>
