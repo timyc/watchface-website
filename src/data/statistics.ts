8aa882c64300ba865b5ed73c8c1d37f1a21320f7
KyleHu14 <lloydpearce4@gmail.com>
2023-05-16 12:59:24 -0700

    Changed Floor Count statistics to Floors

--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,37 +1,37 @@
 export default {
-    "sleep": {
-        name: "Sleep",
-        icon: "sleep",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "7hr 30min"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "30%"
-            },
-            "week": {
-                name: "This Week",
-                text: "30hr 15min"
-            }
-        },
-    },
-    "hrm": {
-        name: "Heart Rate",
-        icon: "heart",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "70"
-            },
-            /*"percent": {
+	sleep: {
+		name: "Sleep",
+		icon: "sleep",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "7hr 30min",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "30%",
+			},
+			week: {
+				name: "This Week",
+				text: "30hr 15min",
+			},
+		},
+	},
+	hrm: {
+		name: "Heart Rate",
+		icon: "heart",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "70",
+			},
+			/*"percent": {
                 name: "Goal (Percentage)",
                 text: "72%"
             },*/
-        },
-    },
-    /*"spo2": {
+		},
+	},
+	/*"spo2": {
         name: "SpO2",
         icon: "heart",
         display_methods: {
@@ -45,104 +45,104 @@ export default {
             },
         },
     },*/
-    "stp": {
-        name: "Steps",
-        icon: "footprint",
-        display_methods: {
-            "today": {
-                name: "Steps Today",
-                text: "18,381"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "97%"
-            },
-            "week": {
-                name: "This week",
-                text: "103,333"
-            }
-        },
-    },
-    "calories": {
-        name: "Calories",
-        icon: "fire",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "192"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "53%"
-            },
-            "week": {
-                name: "This Week",
-                text: "1,291"
-            },
-        },
-    },
-    "distance": {
-        name: "Distance",
-        icon: "distance",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "1.23km"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "23%"
-            },
-            "week": {
-                name: "This Week",
-                text: "12.91km"
-            },
-        },
-    },
-    "azm": {
-        name: "Active Zone Minutes",
-        icon: "floor",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "10min"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "23%"
-            },
-            "week": {
-                name: "This Week",
-                text: "1hr 30min"
-            },
-        },
-    },
-    "weather": {
-        name: "Weather",
-        icon: "weather",
-        display_methods: {
-            "today": {
-                name: "Today",
-                text: "12°C"
-            },
-        },
-    },
-    "flr": {
-        name: "Floor Count",
-        icon: "footprint",
-        display_methods: {
-            "today": {
-                name: "Floor Count Today",
-                text: "1,121"
-            },
-            "percent": {
-                name: "Goal (Percentage)",
-                text: "33%"
-            },
-            "week": {
-                name: "This week",
-                text: "4,888"
-            }
-        },
-    },
-}
+	stp: {
+		name: "Steps",
+		icon: "footprint",
+		display_methods: {
+			today: {
+				name: "Steps Today",
+				text: "18,381",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "97%",
+			},
+			week: {
+				name: "This week",
+				text: "103,333",
+			},
+		},
+	},
+	calories: {
+		name: "Calories",
+		icon: "fire",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "192",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "53%",
+			},
+			week: {
+				name: "This Week",
+				text: "1,291",
+			},
+		},
+	},
+	distance: {
+		name: "Distance",
+		icon: "distance",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "1.23km",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "23%",
+			},
+			week: {
+				name: "This Week",
+				text: "12.91km",
+			},
+		},
+	},
+	azm: {
+		name: "Active Zone Minutes",
+		icon: "floor",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "10min",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "23%",
+			},
+			week: {
+				name: "This Week",
+				text: "1hr 30min",
+			},
+		},
+	},
+	weather: {
+		name: "Weather",
+		icon: "weather",
+		display_methods: {
+			today: {
+				name: "Today",
+				text: "12°C",
+			},
+		},
+	},
+	flr: {
+		name: "Floors",
+		icon: "footprint",
+		display_methods: {
+			today: {
+				name: "Floors Today",
+				text: "1,121",
+			},
+			percent: {
+				name: "Goal (Percentage)",
+				text: "33%",
+			},
+			week: {
+				name: "This week",
+				text: "4,888",
+			},
+		},
+	},
+};
